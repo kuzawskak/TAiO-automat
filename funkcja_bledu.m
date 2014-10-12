a706eb58599bcc96428bcabe5157e96c9c355eb7
unknown <kasia.kuzawska@gmail.com>
2014-10-13 00:59:25 +0200

    save strings to csv format fixed, TODO: funkcja_bledu

---
+++ funkcja_bledu.m
@@ -0,0 +1,22 @@
+%macierz funkcji jest 3-wymiarowa
+%stan wejsciowy to wektor dlugosci liczba_podzialow_na _podprzedzialy
+%%napewno zle
+%%
+function blad = funkcja_bledu(macierz_funkcji,stan_wejsciowy)
+blad = 0;
+n=-1;
+dl_wektora = dim(size(slowo),1);
+for i=1:dl_wektora
+    if(stan_wejsciowy(i)==1)
+        n=i;
+    end
+end
+
+st_wy = macierz_funkcji(n)* stan_wejsciowy;
+%blad liczymy jako sume miejsc na ktorych sie rozni stan odczytany 
+
+
+
+end
+
+
