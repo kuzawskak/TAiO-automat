b0be77a1bbdc3956a402b253d55956df87d9f378
mkornasz <mantus92@gmail.com>
2014-10-17 20:52:54 +0200

    dodany komentarz w mnozeniu macierzy

--- automat_simulation.m
+++ automat_simulation.m
@@ -7,6 +7,7 @@ prev_state=zeros(size(matrix_of_transitions,1),1);
 prev_state(1)=1;
 next_state=prev_state;
 for i=1:length(vector)
+%Narazie zwykle mnozenie, bo dla 0 i 1 to bedzie dzialac, potem trzeba dac wzor od Homendy
     next_state=matrix_of_transitions(:,:,vector(i))*prev_state;
     prev_state=next_state;
 
