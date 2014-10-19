309ebf1573824e51efc72a73ee791ea4f8a6940d
mkornasz <mantus92@gmail.com>
2014-10-19 23:59:15 +0200

    Funkcja bledu, pewnie zla

--- funkcja_bledu.m
+++ funkcja_bledu.m
@@ -1,19 +1,21 @@
-%macierz funkcji jest 3-wymiarowa
-%stan wejsciowy to wektor dlugosci liczba_podzialow_na _podprzedzialy
+%Macierz funkcji jest 3-wymiarowa, wygenerowana wczesniej przez funkcje
+%'generate automat' Zbior uczacy wygenerowany wczesniej przez funkcje
+%'prepare_training_dataset Liczba symboli i Liczba kopii potrzebna do funkcji find_symbol
 
-function blad = funkcja_bledu(macierz_funkcji,stan_wejsciowy)
+function blad = funkcja_bledu(macierz_funkcji, zbior_uczacy, liczba_symboli, liczba_kopii)
+
 blad = 0;
-n=-1;
-dl_wektora = dim(size(slowo),1);
-for i=1:dl_wektora
-    
-    if(stan_wejsciowy(i)==1)
-        n=i;
+wynik=0;
+
+%iterujemy po wszystkich wektorach ze zbioru uczacego
+for i=1:size(zbior_uczacy,1)
+    x=find_symbol(i,liczba_symboli,liczba_kopii);
+    wynik=automat_simulation( zbior_uczacy(i,:), macierz_funkcji );
+    %sprawdzamy czy wynik automatu jest prawidlowy, jesli nie, to zwiekszamy blad 
+    if(x~=wynik)
+        blad=blad+1;
     end
 end
-
-st_wy = macierz_funkcji(n)* stan_wejsciowy;
-%blad liczymy jako sume miejsc na ktorych sie rozni stan odczytany 
 
 end
 
